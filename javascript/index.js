bcaa17eb1d668e6872bf2e6a559071a936b932ed
Felipe Signorelli <signorellifelipe1@gmail.com>
2023-01-10 20:48:03 -0300

    Fin de la primera clase

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+let nombreAsignado = prompt("Hola, ingrese su Nombre por favor");
+
+console.log("Ahora aparece cosas en la consola");
+
+console.log(nombreAsignado);
+
+alert("Ahora te muestro tu nombre mira: "+nombreAsignado);
+
+let x1 =24, x2=55;
+
+let resultado1 = x1+x2;
+
+console.log(resultado1);
