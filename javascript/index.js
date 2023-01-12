aadadfe37d869f4d6a97cd21d96ba9cce838a74a
Felipe Signorelli <signorellifelipe1@gmail.com>
2023-01-12 20:55:58 -0300

    Clase 2

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+/* CLASE 1
+
 let nombreAsignado = prompt("Hola, ingrese su Nombre por favor");
 
 console.log("Ahora aparece cosas en la consola");
@@ -10,4 +12,36 @@ let x1 =24, x2=55;
 
 let resultado1 = x1+x2;
 
-console.log(resultado1);
+console.log(resultado1); */
+//clase 2
+
+/* let nombreUsuario1 = prompt("Ingrese su nombre por favor: ");
+
+if(nombreUsuario1 == ""){
+    alert("No escribiste nada pepega.");
+}
+else{
+    alert("Usted ingreso este nombre: "+nombreUsuario1);
+} */
+
+/* let precio1=39;
+
+if (precio1 < 12){
+    alert("Precio es menor a 12");
+}
+else if(precio1<40){
+    alert("Precio menos a 50");
+}
+else{
+    alert("El precio es re alto no jodas flaco.");
+} */
+let x1 = prompt("Ingrese un numero: ");
+
+let color1 = prompt("Ingrese un color: ");
+
+if (x1>50 &&  color1=="Rojo") {
+    alert("Todo coincide, es rojo y es major a 50");
+}
+else{
+    alert("Algo no coincide.");
+}
