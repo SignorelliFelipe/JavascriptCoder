ed4c7858a13347b27f9a1c036d275069dfbb514f
Felipe Signorelli <signorellifelipe1@gmail.com>
2023-01-17 21:00:25 -0300

    Clase 3

--- javascript/index.js
+++ javascript/index.js
@@ -35,7 +35,7 @@ else if(precio1<40){
 else{
     alert("El precio es re alto no jodas flaco.");
 } */
-let x1 = prompt("Ingrese un numero: ");
+/* let x1 = prompt("Ingrese un numero: ");
 
 let color1 = prompt("Ingrese un color: ");
 
@@ -44,4 +44,67 @@ if (x1>50 &&  color1=="Rojo") {
 }
 else{
     alert("Algo no coincide.");
+}
+ */
+
+//CLASE 3 ITERADORES
+/* let NumeroUsuraio1 = parseInt(prompt("Ingresa un numero flaco: "));
+
+for(let i =0;i<=10;i++){
+    let resultado1 =i *NumeroUsuraio1;
+    console.log(NumeroUsuraio1 + " X "+ i +" = " +resultado1);
+} */
+/* for (let i = 1; i <= 4; i++) {
+    // En cada repetición solicitamos un nombre.
+    let ingresarNombre = prompt("Ingresar nombre");
+    // Informamos el turno asignado usando el número de repetición (i).
+    console.log(" Turno  N° "+i+" Nombre: "+ingresarNombre);
+}
+ */
+
+/* for (let i = 1; i <= 5; i++) {
+
+    let nombre = prompt("Ingresar un nombre: ");
+    //pisa al siguiente
+    if(i==3){
+        continue;
+    }
+
+    if(i == 3){
+        console.log(nombre + " Es el tercero y se detiene ")
+        break;
+    }
+    console.log(nombre +" Es el jugador numero: "+i);
+} */
+
+/* let entrada = prompt("ingresa un dato: "); */
+
+/* while (entrada != "ESC") {
+    console.log(" Usted ingreso: "+entrada);
+    
+    entrada = prompt("ingresa un dato: ");
+} */
+
+/* do{
+    console.log(" Usted ingreso: "+entrada);
+    
+    entrada = prompt("ingresa un dato: ");
+}while(entrada != "ESC") */
+
+
+let entrada = prompt("Ingresar un nombre");
+//Repetimos hasta que se ingresa "ESC"
+while(entrada != "ESC" ){
+   switch (entrada) {
+       case "ANA":
+            alert("HOLA ANA");
+            break;
+        case "JUAN":
+            alert("HOLA JUAN");
+            break;
+       default:
+           alert("¿QUIÉN SOS?")
+           break;
+   }
+   entrada = prompt("Ingresar un nombre");
 }
