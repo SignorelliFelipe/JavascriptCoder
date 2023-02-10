acf51dec7907d6b5f3a2de1a22447e0ab0e5a091
Felipe Signorelli <signorellifelipe1@gmail.com>
2023-02-09 21:09:45 -0300

    Intento de arreglo antes de comer

--- javascript/index.js
+++ javascript/index.js
@@ -38,25 +38,24 @@ Productos.push(new Producto(idsProducos++,"Remera","Blanca",1245));
 //Un  nombres de Clientes random para que supuestamnete ya exista mas gente
 Clientes.push(new Cliente(idsPerersonas++,"Pepe","Argento"));
 
-//Funcion repetida 
+//Funcion repetida basica
 function mostrarArray(array){
-    
+    array.forEach((el)=>{
+        console.log(el);
+    });
 }
 
 
-
 //me presento y doy contexto
-console.log(` Buenos dias, esta es la segunda entrega :D
-Ahora van a poder ver un pueño menu en pantalla
-y en la consola.`);
+console.log("Buenos dias, esta es la segunda entrega :D Ahora van a poder ver un pequeño menu en pantalla y en la consola.");
 
-
-alert(` Buenos dias, esta es la segunda entrega :D
-        Ahora van a poder ver un pueño menu en pantalla
-        y en la consola.`);
-
+alert("Buenos dias, esta es la segunda entrega :D Ahora van a poder ver un pequeño menu en pantalla y en la consola.");
+alert("LEER EL SIGUIENTE MENSAJE");
+//Pongo otra aclaracion por el VS. No se que pasa de verdad que le di vueltas pero no entiendo xq tengo que tocar la ultima opcion para luego darle F5 y recien ahi funciona.
+alert("Si no puede ejercutar el codigo al tocar algun boton toque el numero 7 para SALIR y luego F5. Estuve hablando con tutores y CoderAsk pero sigo sin saber que es lo que ocasiona este inconveniente.");
+ 
 let opcion = parseInt(
-    prompt(`¿Cual opcion va a elegir?
+    prompt(`Buenas ¿Cual opcion va a elegir?
                               1- Ver Productos 
                               2- Agregar producto a la compra
                               3- Ver Total 
@@ -71,22 +70,21 @@ while(opcion !== 7){
     switch (opcion) {
         case 1:
             //Aca muestro el array con el forEach, casi uso un for con el largo del array pero esto hacer el codigo mas limpio
+            console.log("");
             console.log("Mostrando Productos: ");
-            Productos.forEach((el)=>{
-                console.log(el);
-            });
+            mostrarArray(Productos);
             break;
         case 2:
             //El case 2 esta separado del 1 solamente xq hay gente que quiere ve los productos sin mas y no queria mostrar el mensaje de agregar al carrito. 
-            console.log("Mostrando Productos para agregar al carro: ");
-            Productos.forEach((el)=>{
-                console.log(el);
-            });
-            let OpcionAarray = prompt("Ingrese la ID del Producto para agregarlo: ");
             console.log("");
+            console.log("Mostrando Productos para agregar al carro: ");
+            mostrarArray(Productos);
+            let OpcionAarray = prompt("Ingrese la ID del Producto para agregarlo: ");
             CarritoCompras.push(Productos[OpcionAarray]);
             break; 
         case 3:
+            // Ya con el Array de Carrito creado solo sumo los .precio y los muestro en pantalla
+            console.log("");
             CarritoCompras.forEach((el)=>{
                 total+=el.precio;
             });
@@ -99,14 +97,15 @@ while(opcion !== 7){
             
             break;
         case 4:
+            //Un pequeño Buscador que devuelve Arrays con los datos de los Productos
+            console.log("");
             let Buscador=prompt("Ingrese nombre para ser buscado: ")
             const buscarProducto = Productos.filter((el) => el.nombre.includes(Buscador));
-            /* console.log(buscarProducto); */
-            buscarProducto.forEach((el)=>{
-                console.log(el);
-            });
+            mostrarArray(buscarProducto);
             break;
         case 5:
+            //Esto es para que se puedan crear nuevas IDs y nuevos clientes
+            console.log("");
             let opcion1 =prompt("Si ya es cliente escriba ESC, sino escriba cualquier para continuar");
 
             while(opcion1 != "ESC"){
@@ -121,9 +120,9 @@ while(opcion !== 7){
 
             break;
         case 6:
-            Clientes.forEach((el)=>{
-                console.log(el);
-            }); 
+            // Mostrar que el cliente fue ingresado correctamente
+            console.log("");
+            mostrarArray(Clientes);
             break;
         default:
             break;
@@ -139,8 +138,8 @@ while(opcion !== 7){
                                   7- Salir  
       `)
       );
-if (opcion == 7) {
-    alert("Muchas gracias por su tiempo. Vuelva pronto!");
-    console.log("Muchas gracias por su tiempo. Vuelva pronto!");
-}
+    if (opcion == 7) {
+        alert("Muchas gracias por su tiempo. Vuelva pronto!");
+        console.log("Muchas gracias por su tiempo. Vuelva pronto!");
+    }
 }
